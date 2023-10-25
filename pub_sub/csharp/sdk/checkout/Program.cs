9d271f7b6f78a3451d8ff48f67e313c2de10a6db
ngotienluu <ngotienluu@hotmail.com>
2023-10-25 10:30:17 +0700

    add pub sub services

--- pub_sub/csharp/sdk/checkout/Program.cs
+++ pub_sub/csharp/sdk/checkout/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System;
+using Dapr.Client;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+for (int i = 1; i <= 10; i++)
+{
+    var order = new Order(i);
+    using var client = new DaprClientBuilder().Build();
+
+    // publish an event/message using dapr pubsub
+    await client.PublishEventAsync("orderpubsub", "orders", order);
+    Console.WriteLine("publish data: " + order);
+    
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+public record Order([property: JsonPropertyName("orderId")] int OrderId);
